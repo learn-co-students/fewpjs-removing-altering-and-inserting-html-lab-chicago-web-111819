8b9e8d5df63cea54e7e6568e7b273a28264130bd
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-05 17:32:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
-document.querySelector("main#main").remove();
+document.getElementById("main").remove();
 
 let newHeader = document.createElement("h1");
 newHeader.id = "victory"

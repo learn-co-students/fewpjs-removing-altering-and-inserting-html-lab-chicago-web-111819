773e48652a2f3dcdb4237bc1370836fdd6148866
dut-a <d.dut@live.com>
2020-01-05 13:48:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Removing the `main` div
+document.querySelector("#main").remove();
+
+
+let newH1 = document.createElement("h1");
+newH1.setAttribute("id", "victory");
+document.body.appendChild(newH1)
+
+let newHeader = document.querySelector('h1#victory');
+let txt = document.createTextNode("Dut is the champion");
+// Adding text to its container
+newHeader.appendChild(txt);
+
